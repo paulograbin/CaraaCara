473cb60cfdd99baef2d0ded526d3cb60f2c95dad
Paulo Grolli Grabin <paulograbin@gmail.com>
2024-11-23 14:16:56 -0300

    Add missing class

---
+++ src/main/java/com/paulograbin/Assets.java
@@ -0,0 +1,21 @@
+package com.paulograbin;
+
+public class Assets {
+
+    public static String RESOURCE_EMMA_WATSON = "/images/emma_watson.jpg";
+    public static String RESOURCE_EMILIA = "/images/emilia.jpg";
+    public static String RESOURCE_NATALIE = "/images/natalie.jpg";
+    public static String RESOURCE_MILA = "/images/mila.jpg";
+    public static String RESOURCE_SCARLETT = "/images/scarlett.jpg";
+    public static String RESOURCE_EU = "/images/eu.jpg";
+    public static String RESOURCE_OBAMA = "/images/obama.jpg";
+
+    public static class Names {
+        public static final String EMMA_WATSON = "Emma Watson";
+        public static final String EMILIA_CLARKE = "Emilia Clarke";
+        public static final String NATALIE = "Natalie Portman";
+        public static final String SCARLETT = "Scarlett Johansson";
+        public static final String MILA = "Mila Kunis";
+    }
+
+}
