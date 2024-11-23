d79ccf0d9e5ff08a18bb33e05b73a49ff1bd4551
Paulo Grolli Grabin <paulograbin@gmail.com>
2024-11-23 17:09:46 -0300

    Remove comments and rename attributes from Personagem

--- src/main/java/com/paulograbin/Personagem.java
+++ src/main/java/com/paulograbin/Personagem.java
@@ -2,58 +2,40 @@ package com.paulograbin;
 
 import javax.swing.ImageIcon;
 
+
 public class Personagem {
+
     private ImageIcon foto;
     private String nome;
-    private boolean estado;
-    
-    public Personagem(ImageIcon foto, String nome)
-    {
+    private boolean visivel;
+
+    public Personagem(ImageIcon foto, String nome) {
         this.foto = foto;
         this.nome = nome;
-        this.estado = true;
+        this.visivel = true;
     }
-    
-    /**
-     * @return the foto
-     */
+
     public ImageIcon getFoto() {
         return foto;
     }
-    
-    public boolean getEstado()
-    {
-        return estado;
-    }
-    
-    public void setEstado() {
-        if (estado == true)
-        {
-            estado = false;
-        }
-        else
-        {
-            estado = true;
-        }
-    };
 
-    /**
-     * @param foto the foto to set
-     */
+    public boolean getVisivel() {
+        return visivel;
+    }
+
+    public void toggleVisivel() {
+        this.visivel = !this.visivel;
+        System.out.println("Personagem " + nome + " estado " + visivel);
+    }
+
     public void setFoto(ImageIcon foto) {
         this.foto = foto;
     }
 
-    /**
-     * @return the nome
-     */
     public String getNome() {
         return nome;
     }
 
-    /**
-     * @param nome the nome to set
-     */
     public void setNome(String nome) {
         this.nome = nome;
     }
